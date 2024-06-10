231f275f12e623dcb68417d83bc19c6ba412a390
JetHuynh <lochv85@gmail.com>
2024-06-10 22:20:14 +0700

    Exercise module01 week01 - Quiz

---
+++ MODULE01/WEEK01/Quiz/Quiz_ (13).py
@@ -0,0 +1,32 @@
+"""### Câu hỏi 13 : Đoạn code nào thực hiện đúng Mean Difference of nth Root Error được miêu tả ở trên?"""
+
+#(A)
+def md_nre_single_sample (y, y_hat , n, p):
+    y_root = y ** (1/n)
+    y_hat_root = y_hat ** (1/n)
+    difference = y_root - y_hat_root
+    loss = difference ** p
+    return loss
+# (B)
+def md_nre_single_sample1 (y, y_hat , n, p):
+    y_root = y ** (1/n)
+    y_hat_root = y_hat ** (1/2) #wrong >>1/n
+    difference = y_root - y_hat_root
+    loss = difference ** p
+    return loss
+# (C)
+def md_nre_single_sample2 (y, y_hat , n, p):
+    y_root = y ** (1/n)
+    y_hat_root = y_hat ** (1/n)
+    difference = y_root / y_hat_root #wrong >> y-y_hat
+    loss = difference ** p
+    return loss
+# (D)
+def md_nre_single_sample3 (y, y_hat , n, p):
+    y_root = y ** (1/n)
+    y_hat_root = y_hat ** (1/n)
+    difference = y_root - y_hat_root
+    loss = difference #wrong >>diff**p
+    return loss
+
+print('A')
